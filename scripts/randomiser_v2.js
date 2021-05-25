fe898f38052421e22838265eae38a47ffc26f5ea
sammytone23 <sammytone23@gmail.com>
2021-05-26 09:51:19 +1000

    possible mvp?

--- scripts/randomiser_v2.js
+++ scripts/randomiser_v2.js
@@ -1,12 +1,27 @@
 //randomiser v2
+function loadJSON(callback) {
+	var xobj = new XMLHttpRequest();
+	xobj.overrideMimeType("application/json");
+
+	xobj.onreadystatechange = function () {
+			if (xobj.readyState == 4 && xobj.status == "200") {
+				// Required use of an anonymous callback as .open will NOT return 
+				// a value but simply returns undefined in asynchronous mode
+				callback(xobj.responseText);
+			} else {
+				callback("[{\"Nothing\"}]");
+			}
+		};
+
+	xobj.open('GET', '../../data/data.json', true);
+	// Maybe you require use of an unknown origin.
+	/*xobj.setRequestHeader("Access-Control-Allow-Origin","*");*/
+	xobj.send(null);  
+};
+
+
 function load(){
-	let data = JSON.parse(pages)
-	let i=0
-	choice=Math.floor(Math.random()*(data.pages.length))
-	console.log(choice,i)
-	while(choice!=3&&i<=50){
-		i++
-		choice=Math.floor(Math.random()*(data.pages.length))
-		console.log(choice,i)
-	}
+	loadJSON(function(response){
+		
+	});
 }
